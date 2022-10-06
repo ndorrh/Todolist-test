eade2e434fd3addfec8cb18b749ec6a2981c8623
ndorrh <ndorrh@gmail.com>
2022-10-06 05:44:18 +0100

    Add test for Add and remove function

--- src/list.test.js
+++ src/list.test.js
@@ -1,2 +1,16 @@
 import Item from './modules/listItem.js';
 import List from './modules/list.js';
+
+describe('App logic', () => {
+  const list = new List(Item, 'testing list', 'test list sto');
+  test('Expect addItem to increase list length', () => {
+    const listLength = list.list.length;
+    list.addItem('Hola');
+    expect(list.list.length).toBe(listLength + 1);
+  });
+  test('Expect removeItem to increase list length', () => {
+    const listLength = list.list.length;
+    list.removeItemByIndex(0);
+    expect(list.list.length).toBe(listLength - 1);
+  });
+});
