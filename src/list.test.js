08637f6b5189df7e96c40c13b3f355ded4b3c21b
ndorrh <ndorrh@gmail.com>
2022-10-06 05:45:14 +0100

    Add test to check the DOM using mock

--- src/list.test.js
+++ src/list.test.js
@@ -14,3 +14,24 @@ describe('App logic', () => {
     expect(list.list.length).toBe(listLength - 1);
   });
 });
+
+describe('DOM Tests', () => {
+  jest.mock('./index');
+  test('Expect addItem to add to DOM', () => {
+    const list1 = new List(Item, 'testing list0', 'test list sto0');
+    document.body.innerHTML = '<div><ul id="list"></ul></div>';
+    const item = (list1.addItem('Task 1')).template();
+    document.querySelector('#list').appendChild(item);
+    const check = document.querySelectorAll('#list li');
+    expect(check).toHaveLength(1);
+  });
+
+  test('Expect removeItem to remove from DOM', () => {
+    const list1 = new List(Item, 'testing list3', 'test list sto3');
+    document.body.innerHTML = '<div><ul id="list"></ul></div>';
+    const item = list1.addItem('Task 1').template();
+    document.querySelector('#list').appendChild(item).remove();
+    const check = document.querySelectorAll('#list li');
+    expect(check).toHaveLength(0);
+  });
+});
