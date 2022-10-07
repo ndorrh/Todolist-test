dd1d465ce08bc29d2684cc3ac6a49de46dc82415
stanley <tochukwuu14@gmail.com>
2022-10-07 09:31:00 +0200

    Add some codes to list.test.js

--- src/list.test.js
+++ src/list.test.js
@@ -50,7 +50,7 @@ describe('DOM Tests', () => {
     expect(check).toHaveLength(0);
   });
 
-  test('Expect editing task description', () => {
+  test('Expect editing', () => {
     const list1 = new List(Item, 'testing list4', 'test list sto4');
     const initialText = 'This should change';
     document.body.innerHTML = '<div><ul id="list"></ul></div>';
